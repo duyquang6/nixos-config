cf80e1908a9e86fcaf03372b6202fd72a06410f6
ligt <me@ligt.dev>
2025-11-08 17:12:16 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
+    };
+    cursor.url = "github:duyquang6/cursor-nixos-flake";
+  };
+
+  outputs = inputs: {
+
+    nixosConfigurations.default = inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        { nix.settings = { 
+	    experimental-features = ["nix-command" "flakes"]; 
+	    trusted-users = [ "root" "ligt" ];
+	  };}
+        ./hosts/default/configuration.nix
+     	  ./hosts/default/noctalia.nix
+     	  inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
---
+++ hosts/default/configuration.nix
@@ -0,0 +1,290 @@
+ # Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, inputs, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+
+
+  # network misc
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      qt6Packages.fcitx5-unikey
+    ];
+  };
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  # services.xserver.enable = true;
+
+  # gtk.iconCache.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+
+  # enable gnome
+  # services.displayManager.gdm = {
+  #   enable = true;
+  # };
+  # services.desktopManager.gnome.enable = true;
+  services.greetd.enable = true;
+
+  services.greetd.settings = rec {
+    initial_session = {
+      command = "${pkgs.tuigreet}/bin/tuigreet --user-menu --time --remember --cmd '${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri --session'";
+      user = "greeter";
+    };
+    default_session = initial_session;
+  };
+
+  # xdg
+  xdg.portal = {
+    enable = true;    
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+    ];
+  };
+  security.polkit.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ligt = {
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "ligt";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      thunderbird
+      google-chrome
+    ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "ligt" = import ./home.nix;
+    };
+  };
+
+  # Install browsers
+  programs.firefox.enable = true;
+  # install fish 
+  programs.fish.enable = true;
+  programs.gpu-screen-recorder.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # utilities 
+    wget
+    btop
+    mpv
+    git
+    fuzzel
+    foot
+    xwayland-satellite
+
+    # ui
+    capitaine-cursors
+
+    # apps
+    telegram-desktop
+    filezilla
+    yazi
+
+    # dev
+    inputs.cursor.packages.${system}.cursor
+    kitty
+    jujutsu
+    evil-helix
+    devenv
+    vscode
+  ];
+
+  services.flatpak.enable = true;
+
+  # Ensure XWayland is available for X apps under Niri
+  programs.xwayland.enable = true;
+  # programs.hyprland.enable = true;
+  programs.niri.enable = true;
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  environment.variables = {
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    EDITOR = "hx";
+    SUDO_EDITOR = "hx";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "ligt"];
+  };
+  # bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # gpu
+  boot.extraModprobeConfig = ''
+      options nvidia_modeset vblank_sem_control=0 nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp
+      '';
+  boot.kernelModules = ["nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nvidia"];
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+  services.xserver = {
+    videoDrivers = [ "nvidia" ]; 
+  };
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true; #without this after suspend was black screen without cursor; when true, cursor appears
+    powerManagement.finegrained = false;
+    open = true;
+    # package = config.boot.kernelPackages.nvidiaPackages.latest;
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "570.195.03";
+    #   sha256_64bit = "sha256-1H3oHZpRNJamCtyc+nL+nhYsZfJyL7lgxPUxvXrF3B4=";
+    #   sha256_aarch64 = "sha256-o4rgB6vo+Cv90lJywovIyVARRGS3R15zYQUj+f1nzWQ=";
+    #   openSha256 = "sha256-vCBB/UJgVKHlSEWdgoF45lODr3YJmR6JwjrwWgWszBw=";
+    #   settingsSha256 = "sha256-mjKkMEPV6W69PO8jKAKxAS861B82CtCpwVTeNr5CqUY=";
+    #   persistencedSha256 = "sha256-BMpo2PIabhHjZQqUQi/W5DYhgAPmfCdFvXdN6ND2Bfs=";
+    # };
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+  ];
+
+
+  # programs.obs-studio = {
+  #   enable = true;
+  #
+  #   # optional Nvidia hardware acceleration
+  #   package = (
+  #     pkgs.obs-studio.override {
+  #       cudaSupport = true;
+  #     }
+  #   );
+  #
+  #   plugins = with pkgs.obs-studio-plugins; [
+  #     wlrobs
+  #     obs-backgroundremoval
+  #     obs-pipewire-audio-capture
+  #     obs-vaapi #optional AMD hardware acceleration
+  #     obs-gstreamer
+  #     obs-vkcapture
+  #   ];
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
